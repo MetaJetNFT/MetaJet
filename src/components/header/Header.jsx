046dcce8df2f76ddaeab245cf1eb86570a5edc2d
Catherine <oceanbeaats@gmail.com>
2022-04-01 20:25:09 -0700

    Update Header.js

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header({ fixed }) {
             <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-pink"
               href="#pablo"> </a>
             <button className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-pink rounded bg-pink block lg:hidden outline-none focus:outline-none"
-              type="button" onClick={() => setHeaderOpen(!headerOpen)}><FaBars color='pink'/></button>
+              type="button" onClick={() => setHeaderOpen(!headerOpen)}><FaBars color='#8122b2'/></button>
           </div>
           <div className={"lg:flex flex-grow items-center" +
               (headerOpen ? " flex" : " hidden")}
@@ -21,13 +21,13 @@ export default function Header({ fixed }) {
 
                 <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="#crewpass" class="block mt-4 lg:inline-block lg:mt-0 text-pink-200 hover:text-white mr-4">
+      <a href="#crewpass" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4">
         Crewpass
       </a>
-      <a href="#roadmap" class="block mt-4 lg:inline-block lg:mt-0 text-pink-200 hover:text-white mr-4">
+      <a href="#roadmap" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4">
         RoadMap
       </a>
-      <a href="#team" class="block mt-4 lg:inline-block lg:mt-0 text-pink-200 hover:text-white mr-4">
+      <a href="#team" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4">
         Team
       </a>
     
