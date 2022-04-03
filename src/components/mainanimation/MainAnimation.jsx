07c43bc2c13210aed9e10d6b5459e6357ef3121a
Catherine <oceanbeaats@gmail.com>
2022-04-03 16:23:07 -0700

    .

--- src/components/mainanimation/MainAnimation.jsx
+++ src/components/mainanimation/MainAnimation.jsx
@@ -6,7 +6,7 @@ function MainAnimation() {
   return (
      <div
           dangerouslySetInnerHTML={{
-            __html: `<video className="aspect-w-16 aspect-h-9" autoplay loop muted playsinline>
+            __html: `<video class="aspect-w-16 aspect-h-9 lg:aspect-full" autoplay loop muted playsinline>
       <source src=${Metajet} type="video/mp4" />
       Your browser does not support the video tag.
 </video>`,
