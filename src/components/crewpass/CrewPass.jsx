c888b6c4dba4e1b7ec37fa650a0e0eb124a2c607
catherinekomi <catherine@alopexid.com>
2022-04-09 23:25:55 -0700

    added footer

--- src/components/crewpass/CrewPass.jsx
+++ src/components/crewpass/CrewPass.jsx
@@ -6,8 +6,8 @@ const CrewPass = () => (
   <div className="gpt3__header section__padding" id="crewpass">
     <div className="gpt3__header-content">
       <h1 className="neonlogo">CREW PASS</h1>
-      <h3 >The Crew Pass is the foundation of MetaJet and our future endeavors.  It will provide assets for its holders that will not be available for non crew pass holders.  MetaJet is here to stay and provide unparalleled utility to our earliest supporters, that is why 25% of all royalties MetaJet makes off of sales will be airdropped back to our Crew Pass holders monthly.  The Crew Pass will provide automatic whitelist and presale access to collection one and two and enter each holder into exclusive raffles and giveaways.  Holding the Crew Pass will also give each holder access to an exclusive merchandise store created by professional designers.  And wait… there’s more, holding 2x or more Crew Passes will get you a free mint of Collection One.  </h3>
-
+      <h4>50% of all royalties from Crew Passes will go back to the Crew Pass holders.  We would like to airdrop it through a monthly snapshot but we are still working through the legality of that.  In the meantime, it will get back to our holders in one way, whether that's through buying whitelist spots, NFT giveaways, or whatever other way our community can think.  </h4>
+    
 
     </div>
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,8 +12,9 @@ export default function Header({ fixed }) {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-black-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-pink"
-              href="#pablo"> </a>
+            <a className="leading-relaxed inline-block mr-4 py-2"> </a>
+
+              {/* button on mobile */}
             <button className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-pink rounded bg-pink block lg:hidden outline-none focus:outline-none"
               type="button" onClick={() => setHeaderOpen(!headerOpen)}><FaBars color='#8122b2'/></button>
           </div>
@@ -22,43 +23,43 @@ export default function Header({ fixed }) {
             id="example-navbar-danger">
 
               <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-sm lg:flex-grow">
+         <div class="text-sm lg:flex-grow">
       <a href="#crewpass" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4" className="neonlogo">
-        Crewpass
-      </a>
+        Crewpass 
+        </a>
       <a href="#roadmap" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4" className="neonlogo">
         RoadMap
       </a>
       <a href="#team" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4" className="neonlogo">
         Team
-      </a>
-    
-    </div>
-    </div>
-                
-
-
-        <div class="text-sm lg:flex-grow">
-         <li class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4">
-       <SocialIcon url="https://twitter.com/MetaJetNFT" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4">
-       <SocialIcon url="https://www.instagram.com/metajetnft.io/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4"> 
-       <SocialIcon url="https://discord.com/invite/Rkx5Zkc2QH" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-        <li class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4">
-       <SocialIcon url="https://www.linkedin.com/company/meta-jet/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
+        </a>
         </div>
 
-         <div>
-              <a href="#responsive-header" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-white hover:text-pink-500 hover:bg-black mt-4 lg:mt-0">Marketplace</a>
-                 </div>
+        
+        </div>
+                
 
-            
-          </div>
+      
+      <div class="text-sm lg:flex-grow">
+         <li class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+       <SocialIcon url="https://twitter.com/MetaJetNFT" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+         <li class="block mt-4 lg:inline-block lg:mt-0  mr-4">
+       <SocialIcon url="https://www.instagram.com/metajetnft.io/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+         <li class="block mt-4 lg:inline-block lg:mt-0  mr-4"> 
+       <SocialIcon url="https://discord.com/invite/Rkx5Zkc2QH" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+        <li class="block mt-4 lg:inline-block lg:mt-0  mr-4">
+       <SocialIcon url="https://www.linkedin.com/company/meta-jet/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+      </div>
+
+            <div>
+              <a href="#responsive-header" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-white hover:text-pink-500 hover:bg-black mt-4 lg:mt-0">Marketplace</a>
+            </div>
+   
+      </div>
         </div>
       </nav>
     </>
