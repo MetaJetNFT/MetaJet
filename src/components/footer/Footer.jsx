127b53b080e7d06a913c565dff1365ead3a34adb
catherinekomi <catherine@alopexid.com>
2022-04-17 14:18:02 -0700

    footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,15 +6,17 @@ function Footer() {
     <div class="w-full min-h-fit flex items-center justify-center black py-10">
         <div class="md:w-2/3 w-full px-4 text-white flex flex-col">
             <div class="flex flex-col">
-                <div class="flex mt-1 mb-12 flex-row justify-between">
-
-                    <a href="#crewpass" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">About Us</a>
-                    <a href="#roadmap" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">Road Map</a>
-                    <a href="#team" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">Team</a>
+                <hr class="border-gray-600"/>
+                <div class="flex mt-4 mb-10 flex-row justify-center">
+                    
+                    <a href="#crewpass" class="mx-10 hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">About Us</a>
+                    <a href="#roadmap" class="mx-10 hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">Road Map</a>
+                    <a href="#team" class="mx-10 hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">Team</a>
                   
                 </div>
-                <div class="text-sm lg:flex-grow py-6">
-         <li class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+
+                <div class="flex mt-1 mb-12 flex-row justify-center">
+         <li class="mt-4 lg:inline-block lg:mt-0 mr-4">
        <SocialIcon url="https://twitter.com/MetaJetNFT" bgColor="#fff" style={{ height: 40, width: 40 }}/>
         </li>
          <li class="block mt-4 lg:inline-block lg:mt-0  mr-4">
