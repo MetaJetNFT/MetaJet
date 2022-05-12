05fa82655848a405a2f974d9b260ffaafd531a35
Catherine Komi <catherine@alopexid.com>
2022-05-11 21:44:54 -0700

    crewpass and navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,68 +1,73 @@
-import React from 'react'
 import 'font-awesome/css/font-awesome.min.css';
 import { FaBars } from 'react-icons/fa';
 import { SocialIcon } from 'react-social-icons';
+import React, { useState } from "react";
 
-
-
-export default function Header({ fixed }) {
+const Header = () => {
   const [headerOpen, setHeaderOpen] = React.useState(false);
   return (
-    <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-black-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a href="#roadmap" className="leading-relaxed inline-block mr-4 py-2"> </a>
+    <nav className="relative flex flex-wrap py-3">
+      {/* button on mobile */}
+      <button
+        className="text-white mx-auto cursor-pointer text-xl leading-none px-3 py-1 border border-solid block lg:hidden outline-none focus:outline-none"
+        type="button"
+        onClick={() => setHeaderOpen(!headerOpen)}
+      >
+        <FaBars color="#FFFF" />
+      </button>
 
-              {/* button on mobile */}
-            <button className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-pink rounded bg-pink block lg:hidden outline-none focus:outline-none"
-              type="button" onClick={() => setHeaderOpen(!headerOpen)}><FaBars color='#8122b2'/></button>
-          </div>
-          <div className={"lg:flex flex-grow items-center" +
-              (headerOpen ? " flex" : " hidden")}
-            id="example-navbar-danger">
-
-              <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-         <div class="text-sm lg:flex-grow">
-      <a href="#crewpass" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4" className="neonlogo">
-        Crewpass 
+      <div
+        className={
+          "lg:flex flex-grow lg:justify-center items-center" +
+          (headerOpen ? " flex flex-col" : " hidden")
+        }
+      >
+        <a href="#crewpass" className="neonlogo">
+          Crewpass
         </a>
-      <a href="#roadmap" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4" className="neonlogo">
-        RoadMap
-      </a>
-      <a href="#team" class="block mt-4 lg:inline-block lg:mt-0 text-purple-600 hover:text-white mr-4" className="neonlogo">
-        Team
+        <a href="#roadmap" className="neonlogo">
+          RoadMap
         </a>
+        <a href="#team" className="neonlogo">
+          Team
+        </a>
+        <div>
+          <SocialIcon
+            className="mr-4"
+            url="https://twitter.com/MetaJetNFT"
+            bgColor="#fff"
+            style={{ height: 40, width: 40 }}
+          />
+          <SocialIcon
+            className="mr-4"
+            url="https://www.instagram.com/metajetnft.io/"
+            bgColor="#fff"
+            style={{ height: 40, width: 40 }}
+          />
+          <SocialIcon
+            className="mr-4"
+            url="https://discord.com/invite/Rkx5Zkc2QH"
+            bgColor="#fff"
+            style={{ height: 40, width: 40 }}
+          />
+          <SocialIcon
+            className="mr-4"
+            url="https://www.linkedin.com/company/meta-jet/"
+            bgColor="#fff"
+            style={{ height: 40, width: 40 }}
+          />
         </div>
-
-        
+        <div>
+          <a
+            href="#"
+            class="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-white hover:text-pink-500 hover:bg-black mt-4 lg:mt-0"
+          >
+            Marketplace
+          </a>
         </div>
-                
-
-      
-      <div class="text-sm lg:flex-grow">
-         <li class="block mt-4 lg:inline-block lg:mt-0 mr-4">
-       <SocialIcon url="https://twitter.com/MetaJetNFT" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li class="block mt-4 lg:inline-block lg:mt-0  mr-4">
-       <SocialIcon url="https://www.instagram.com/metajetnft.io/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li class="block mt-4 lg:inline-block lg:mt-0  mr-4"> 
-       <SocialIcon url="https://discord.com/invite/Rkx5Zkc2QH" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-        <li class="block mt-4 lg:inline-block lg:mt-0  mr-4">
-       <SocialIcon url="https://www.linkedin.com/company/meta-jet/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
       </div>
-
-            <div>
-              <a href="#responsive-header" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-white hover:text-pink-500 hover:bg-black mt-4 lg:mt-0">Marketplace</a>
-            </div>
-   
-      </div>
-        </div>
-      </nav>
-    </>
+    </nav>
   );
 }
 
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#1b0e1a',
-      'purple': '#8122b2',
-      'pink': '#dc00f0',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#1b0e1a",
+      purple: "#8122b2",
+      pink: "#dc00f0",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
     },
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
