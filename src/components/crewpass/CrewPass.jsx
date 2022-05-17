918fe62a6a638f769b4f568847cb03ba6316bbd1
Catherine Komi <catherine@alopexid.com>
2022-05-17 15:55:55 -0700

    major changes

--- src/components/crewpass/CrewPass.jsx
+++ src/components/crewpass/CrewPass.jsx
@@ -6,7 +6,7 @@ const CrewPass = () => (
   <div className="gpt3__header section__padding" id="crewpass">
     <div className="gpt3__header-content">
       <h1 className="neonlogo">CREW PASS</h1>
-      <h4>50% of all royalties from Crew Passes will go back to the Crew Pass holders.  We would like to airdrop it through a monthly snapshot but we are still working through the legality of that.  In the meantime, it will get back to our holders in one way, whether that's through buying whitelist spots, NFT giveaways, or whatever other way our community can think.  </h4>
+      <p>50% of all royalties from Crew Passes will go back to the Crew Pass holders.  We would like to airdrop it through a monthly snapshot but we are still working through the legality of that.  In the meantime, it will get back to our holders in one way, whether that's through buying whitelist spots, NFT giveaways, or whatever other way our community can think.  </p>
     
 
     </div>
--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -12,149 +12,233 @@ import { SocialIcon } from 'react-social-icons';
 function Team() {
   return (
     <section id="team">
-    <h1 className="neonlogo">Our Team</h1>
-    <div className="container">
+      <h1 className="neonlogo">Our Team</h1>
+      <div className="teamcontainer">
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src={Cole} alt="" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Cole
+                <br />
+                <span>Co-Founder</span>
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li className="1">
+              <SocialIcon
+                url="https://linkedin.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="2">
+              <SocialIcon
+                url="https://instagram.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="3">
+              <SocialIcon
+                url="https://discord.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+          </ul>
+        </div>
 
-    
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src={Ethan} alt="" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Ethan
+                <br />
+                <span>Co-Founder</span>
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li className="1">
+              <SocialIcon
+                url="https://linkedin.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="2">
+              <SocialIcon
+                url="https://instagram.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="3">
+              <SocialIcon
+                url="https://discord.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+          </ul>
+        </div>
 
-      <div className="card">
-      <div className="content">
-      <div className="imgBx">
-        <img src={Cole} alt="" />
-      </div>
-      <div className="contentBx">
-        <h3>Cole<br/><span>Co-Founder</span></h3>
-      </div>
-      </div>
-      <ul className='sci'>
-        <li className='1'>
-       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='2'>
-       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='3'> 
-       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-      </ul>
-      </div>
-
-       <div className="card">
-      <div className="content">
-      <div className="imgBx">
-        <img src={Ethan} alt="" />
-      </div>
-      <div className="contentBx">
-        <h3>Ethan<br/><span>Co-Founder</span></h3>
-      </div>
-      </div>
-      <ul className='sci'>
-        <li className='1'>
-       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='2'>
-       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='3'> 
-       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-      </ul>
-      </div>
-
-       <div className="card">
-      <div className="content">
-      <div className="imgBx">
-        <img src={Luke} alt="" />
-      </div>
-      <div className="contentBx">
-        <h3>Luke<br/><span>Co-Founder</span></h3>
-      </div>
-      </div>
-      <ul className='sci'>
-        <li className='1'>
-       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='2'>
-       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='3'> 
-       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-      </ul>
-      </div>
-    </div>
-
-    <div className="container">
-
-      <div className="card">
-      <div className="content">
-      <div className="imgBx">
-        <img src={Andrew} alt="" />
-      </div>
-      <div className="contentBx">
-        <h3>Andrew<br/><span>Artist/Graphic Designer</span></h3>
-      </div>
-      </div>
-      <ul className='sci'>
-        <li className='1'>
-       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='2'>
-       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='3'> 
-       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-      </ul>
-      </div>
-
-      <div className="card">
-      <div className="content">
-      <div className="imgBx">
-        <img src={Catherine} alt="" />
-      </div>
-      <div className="contentBx">
-        <h3>Catherine<br/><span>Frontend Developer</span></h3>
-      </div>
-      </div>
-      <ul className='sci'>
-        <li className='1'>
-       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='2'>
-       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='3'> 
-       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-      </ul>
-      </div>
-
-      <div className="card">
-      <div className="content">
-      <div className="imgBx">
-        <img src={Curtis} alt="" />
-      </div>
-      <div className="contentBx">
-        <h3>Kurtis aka OutKrigh<br/><span>Fashion Designer</span></h3>
-      </div>
-      </div>
-      <ul className='sci'>
-        <li className='1'>
-       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='2'>
-       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-         <li className='3'> 
-       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
-        </li>
-      </ul>
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src={Luke} alt="" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Luke
+                <br />
+                <span>Co-Founder</span>
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li className="1">
+              <SocialIcon
+                url="https://linkedin.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="2">
+              <SocialIcon
+                url="https://instagram.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="3">
+              <SocialIcon
+                url="https://discord.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+          </ul>
+        </div>
       </div>
 
+      <div className="teamcontainer">
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src={Andrew} alt="" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Andrew
+                <br />
+                <span>Artist/Graphic Designer</span>
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li className="1">
+              <SocialIcon
+                url="https://linkedin.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="2">
+              <SocialIcon
+                url="https://instagram.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="3">
+              <SocialIcon
+                url="https://discord.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+          </ul>
+        </div>
+
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src={Catherine} alt="" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Catherine
+                <br />
+                <span>Frontend Developer</span>
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li className="first">
+              <SocialIcon
+                url="https://linkedin.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="3">
+              <SocialIcon
+                url="https://discord.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+          </ul>
+        </div>
+
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src={Curtis} alt="" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Kurtis aka OutKrigh
+                <br />
+                <span>Fashion Designer</span>
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li className="1">
+              <SocialIcon
+                url="https://linkedin.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="2">
+              <SocialIcon
+                url="https://instagram.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+            <li className="3">
+              <SocialIcon
+                url="https://discord.com/"
+                bgColor="#fff"
+                style={{ height: 35, width: 35 }}
+              />
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default Team
